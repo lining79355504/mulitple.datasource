24cd8c2d9ee0e142c5e8205e5f35d1af08a09aef
lining17 <lining17@meituan.com>
2018-03-05 19:11:16 +0800

    commit

--- src/main/java/com/dbUtil/multiple/MultipleDataSource.java
+++ src/main/java/com/dbUtil/multiple/MultipleDataSource.java
@@ -42,7 +42,7 @@ public class MultipleDataSource  extends AbstractRoutingDataSource implements Ap
         beanDefinitionRegistry.registerBeanDefinition("multipleDataSourceHandler", definition);
 
         BeanNameAutoProxyCreator beanNameAutoProxyCreator = new BeanNameAutoProxyCreator();
-        beanNameAutoProxyCreator.setBeanNames("dataSourceSwitchAutoProxy");
+        beanNameAutoProxyCreator.setBeanNames("*");
         beanNameAutoProxyCreator.setInterceptorNames("multipleDataSourceHandler");
         BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(BeanNameAutoProxyCreator.class);
         BeanDefinition beanDefinition=beanDefinitionBuilder.getBeanDefinition();
